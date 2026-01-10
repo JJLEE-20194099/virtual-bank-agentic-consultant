90872fb577f103683629467342538d543ea4f2a7
JJLEE-20194099 <long.lt194099@gmail.com>
2026-01-10 09:53:22 +0100

    add producer

---
+++ backend/app/service/nlp/engine.py
@@ -0,0 +1,28 @@
+def analyze(text: str):
+    
+    """
+    I implemented naive method to test pipeline
+    """
+    
+    intents = []
+    entities = []
+
+    t = text.lower()
+
+    if "invest" in t or "investment" in t:
+        intents.append("INVESTMENT_DISCUSSION")
+
+    if "mortgage" in t or "loan" in t:
+        intents.append("CREDIT_DISCUSSION")
+
+    if "real estate" in t:
+        entities.append({
+            "type": "ASSET_CLASS",
+            "value": "REAL_ESTATE"
+        })
+
+    return {
+        "intents": intents,
+        "entities": entities,
+        "risk_keywords": ["invest", "return"] if intents else []
+    }
