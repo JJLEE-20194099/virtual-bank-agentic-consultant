6fabeb74d520cb9e5597a9e39fdd19fe86c28fe6
JJLEE-194099 <long.lt194099@gmail.com>
2026-01-10 20:04:29 +0000

    auth gitname

--- backend/app/service/stt/asr.py
+++ backend/app/service/stt/asr.py
@@ -5,6 +5,7 @@ print("Loading model...")
 model = whisper.load_model("base")
 print("Loaded model")
 
+
 def transcribe_pcm16(pcm_bytes: bytes, sample_rate=16000):
     audio = np.frombuffer(pcm_bytes, dtype=np.int16).astype(np.float32) / 32768.0
     result = model.transcribe(audio, language="en", task="transcribe", fp16=False)
